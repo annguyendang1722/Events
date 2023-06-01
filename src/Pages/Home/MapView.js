ec60e4ce6b19966309d46ebc982ac7362b91c603
an.nguyendang1722 <an.nguyendang1722@gmail.com>
2023-06-01 09:33:20 +0700

    update map

--- src/Pages/Home/MapView.js
+++ src/Pages/Home/MapView.js
@@ -29,7 +29,7 @@ export default function MapView() {
         </Box>
       </Modal>
       
-        <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
+        <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
         
 
 
