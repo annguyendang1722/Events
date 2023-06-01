37d27efd4b5bb1790c8e730a7e469dd74fb299e0
an.nguyendang1722 <an.nguyendang1722@gmail.com>
2023-06-01 09:56:00 +0700

    update web reactJS

--- src/Pages/Home/MapView.js
+++ src/Pages/Home/MapView.js
@@ -30,11 +30,7 @@ export default function MapView() {
       </Modal>
       
         <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
-        
-
-
-   
-      
+            
       </Box>
     </Container>
   );
